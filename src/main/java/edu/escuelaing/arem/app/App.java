dc338e2037f1e5aaa3876b1798671dd149eb28e8
Johan Rueda <johan.rueda@mail.escuelaing.edu.co>
2021-02-17 15:48:28 -0500

    clima

---
+++ src/main/java/edu/escuelaing/arem/app/App.java
@@ -0,0 +1,40 @@
+package edu.escuelaing.arem.app;
+
+import spark.Request;
+import spark.Response;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+import static spark.Spark.*;
+import com.google.gson.Gson;
+/**
+ * Hello world!
+ *
+ */
+public class App
+{
+    public static void main( String[] args )  {
+        port(getPort());
+        Gson gson = new Gson();
+        get("/clima",(req,res) -> {
+            String lugar = req.queryParams("lugar");
+            String conexion = Clima.getClima(lugar);
+            return gson.toJson(conexion);
+        });
+
+    }
+
+
+
+
+    /**
+     * metodo que nos retorna un puerto
+     * @return entero que indica el puerto
+     */
+    static int getPort() {
+        if (System.getenv("PORT") != null) {
+            return Integer.parseInt(System.getenv("PORT"));
+        }
+        return 4567;
+    }
+}
---
+++ src/main/java/edu/escuelaing/arem/app/Clima.java
@@ -0,0 +1,38 @@
+package edu.escuelaing.arem.app;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+public class Clima {
+
+
+
+    public static String getClima (String lugar) {
+        StringBuffer respuesta = new StringBuffer();
+        try{
+            URL url = new URL("https://api.openweathermap.org/data/2.5/weather?q="+lugar+"&appid=f30cfe4149c9d630e1bc1b2a2410c27a");
+            HttpURLConnection conexion = (HttpURLConnection) url.openConnection();
+            conexion.setRequestMethod("GET");
+
+            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(conexion.getInputStream()));
+            String line = bufferedReader.readLine();
+
+            while ((line != null)){
+                respuesta.append(line);
+                line = bufferedReader.readLine();
+            }
+            bufferedReader.close();
+            return String.valueOf(respuesta);
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return String.valueOf(respuesta);
+    }
+
+}
